ad0402a1c08174ab54b5b276f8a64375bf1ff7d0
phillyai <sonagi.philly@gmail.com>
2017-11-27 19:12:34 +0900

    POST 응답 추가

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, Response
+from flask import Flask, Response, jsonify
 from datetime import datetime
-import json
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,10 +9,10 @@ def homepage():
     <p>It is currently {time}.</p>
     """.format(time=datetime.now())
 
-@app.route('/slack/command/meal')
+@app.route('/slack/command/meal', methods=['POST'])
 def meal():
     msg = {'text': '너에게 알려줄 급식따윈 없다'}
-    return Response(json.dumps(msg), mimetype='application/json')
+    return jsonify(msg)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
