15ff4fa91b24e543cff168fff7d28767e1a49450
phillyai <sonagi.philly@gmail.com>
2017-11-27 19:47:52 +0900

    급식 알려주는 거 추가

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, Response, jsonify
 from datetime import datetime
+import urllib.request
+import json
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,9 +13,36 @@ def homepage():
 
 @app.route('/slack/command/meal', methods=['POST'])
 def meal():
+    date = datetime.now()
+    url = f'http://dsm2015.cafe24.com:3000/meal/{date.strftime("%Y-%m-%d")}'
+    res = json.loads(urllib.request.urlopen(url).read())
+
     msg = {
         'response_type': 'in_channel',
-        'text': '너에게 알려줄 급식따윈 없다'
+        "attachments": [
+            {
+                "fallback": "",
+                "pretext": "오늘자 급식:rice:",
+                "fields": [
+                    {
+                        "title": "아침",
+                        "value": '\n'.join(res['breakfast']),
+                        "short": True
+                    },
+                    {
+                        "title": "점심",
+                        "value": '\n'.join(res['lunch']),
+                        "short": True
+                    },
+                    {
+                        "title": "저녁",
+                        "value": '\n'.join(res['dinner']),
+                        "short": True
+                    }
+                    ],
+                "color": "#7CD197"
+            }
+        ]
     }
     return jsonify(msg)
 
