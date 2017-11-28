ac6b0eda1f707427a1e2476ff3bb994651af1dd6
phillyai <sonagi.philly@gmail.com>
2017-11-28 15:58:27 +0900

    payload 키로 사용

--- app.py
+++ app.py
@@ -19,17 +19,16 @@ def homepage():
 
 @app.route('/slack/confirm', methods=['POST'])
 def confirm():
-    print(request.json)
-    print(json.loads(request.json))
-    # if request.form.get('token') == VERIFICATION_TOKEN:
-    chan = request.form.get('channel').get('name')
-    answ = request.form.get('actions')[0].get('value') == 'yes'
-    msg = {
-
-        "text": '응 안전하지 않아~' if answ else '잘 아는군'
-    }
-    return Response(msg, mimetype='application/json')
-    # print(f'토큰이 다름!!\n받은거: {request.form.get("token")}\n예상한거: {VERIFICATION_TOKEN}')
+    form = request.form['payload']
+    print(form)
+    if form.get('token') == VERIFICATION_TOKEN:
+        chan = form.get('channel').get('name')
+        answ = form.get('actions')[0].get('value') == 'yes'
+        msg = {
+            "text": '응 안전하지 않아~' if answ else '잘 아는군'
+        }
+        return Response(msg, mimetype='application/json')
+    print(f'토큰이 다름!!\n받은거: {request.form.get("token")}\n예상한거: {VERIFICATION_TOKEN}')
 
 @app.route('/slack/command/meal', methods=['POST'])
 def meal():
